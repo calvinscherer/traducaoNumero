6a45e50db20147141f969c2c4f83529aa7e76929
Calvin Bersch Scherer <calvin.scherer@gmail.com>
2019-12-11 18:27:47 -0300

    Projeto criado

---
+++ src/main/java/com/br/calvin/TradutorNumeroApplication.java
@@ -0,0 +1,13 @@
+package com.br.calvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradutorNumeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradutorNumeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/calvin/TradutorNumeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.calvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradutorNumeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
